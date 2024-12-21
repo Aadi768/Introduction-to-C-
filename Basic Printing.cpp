4ae1a8dfbeeca60cda7f9d973def504e91dccb58
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-21 09:47:30 +0530

    Add files via upload

---
+++ Basic Printing.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+ cout<<"Hello World";
+ cout<<endl;
+ cout<<"Hello CW"<<endl ;
+ cout<<"The C++ File is created"<<endl<<"Learning Endline";      // it will print anything in b/w " " it will not take it as a function
+
+}
