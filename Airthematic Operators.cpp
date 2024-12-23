b0aab50c0f1df856a3eac9fdfe92c75dba98d8ff
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-23 10:30:00 +0530

    Add files via upload

---
+++ Airthematic Operators.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int x = 5, y = 2;
+   cout << x+y << endl;
+   cout << x-y << endl;
+   cout << x*y << endl;
+   cout << x/y << endl; // this will give value 2 instead of 2.5 cause i use int data type in place of float
+   cout << x%y << endl; // Modulous Operator
+}
