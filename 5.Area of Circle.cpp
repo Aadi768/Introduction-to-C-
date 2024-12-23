7d9849ba6f2bb3dc3f4fec1afcc3ec0504a22e35
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-23 10:49:49 +0530

    Add files via upload

---
+++ 5.Area of Circle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a ;
+    cout << "Enter Radius .: " ;
+    cin >> a ;
+    
+    cout << "Area of Circle is " << 3.145*a*a << endl ; 
+}
