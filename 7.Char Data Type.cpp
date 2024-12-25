676c5f2c823df78ae212e2d909700638b8d83323
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-25 22:28:24 +0530

    Add files via upload

---
+++ 7.Char Data Type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+   // 1st Section
+   char ch = 'a';
+   cout << ch << endl;
+
+  // 2nd Section
+  char ch1 ;
+  cin >> ch1 ;
+  cout << (int)ch1;
+
+
+
+}
