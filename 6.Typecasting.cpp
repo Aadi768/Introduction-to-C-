d845762b7876e3025077ba61b10f124621b6eae4
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-25 08:16:58 +0530

    Update 6.Typecasting.cpp

--- 6.Typecasting.cpp
+++ 6.Typecasting.cpp
@@ -6,14 +6,14 @@ int main(){
 	int y ;
 	cin >> y ;
 	float y1 = y ;  // we just store y into y1 (data loss can occur sometime)
-    cout << y1/2 << endl ;
+        cout << y1/2 << endl ;
     
  // explicit typecasting
 	int x ;    
-    cin >> x ;        
-    float x1 = (float)x ;  // (float)x mean i want to convert x to float
-                          //  float x1 signify that x1 variable is float 
-    cout << x1/2 << endl;
+        cin >> x ;        
+        float x1 = (float)x ;  // (float)x mean i want to convert x to float
+                              //  float x1 signify that x1 variable is float 
+        cout << x1/2 << endl;
     
     // heirarchy of operator 
       cout << "Heirarchy of operator is " ;
