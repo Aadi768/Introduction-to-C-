645abef6ca76ec6dcd068388d63b4c370192e3c2
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-26 23:28:38 +0530

    Update 9.Profit Loss.cpp

--- 9.Profit Loss.cpp
+++ 9.Profit Loss.cpp
@@ -1,3 +1,7 @@
+/* Ques:- If cost price and selling price of an item is input through 
+	  the keyboard, write a program to determine whether the seller has made
+	  profit or incurred loss or no profit or no loss. Also dtermine how much 
+	  profit he made or loss he incurred. */
 #include<iostream>
 using namespace std ;
 int main(){
