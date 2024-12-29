bbc48749d0646a3fbbfb2058bbd5b4a1870a7001
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-29 11:14:09 +0530

    Update 15.Ques_Else-if.cpp

--- 15.Ques_Else-if.cpp
+++ 15.Ques_Else-if.cpp
@@ -3,14 +3,14 @@
 int main() {
 	
 	float x ;
-	std::cout << "Enter the Marks .: " ;
+	std::cout << "Enter the Marks .: " ;  // input
 	std::cin >> x ;
 	
 	if ( x<=100 and  x>=81 ) { 
-		std::cout << "Very Good" ;
+		std::cout << "Very Good" ; 
 	}
 	else if ( x<=80 and x>=61 ) {
-		std::cout << "Good" ;
+		std::cout << "Good" ;   //else if ladder
 	}
 	else if  (  x<=60 and x>= 41 ) {
 		std::cout << "Average" ;
