27ff71d29fd52ab5164852b16bc9b6e6cad40f57
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-29 12:44:56 +0530

    Update 18.Ternary Operator.cpp

--- 18.Ternary Operator.cpp
+++ 18.Ternary Operator.cpp
@@ -1,10 +1,12 @@
+// use generally when you only have if and else don't use in case of else if
 #include <iostream>
 
 int main() {
 
+	int n ;
+	std::cout << "Enter The Num -- " ;
+	std::cin >> n ;
+	// (condition) ? if true : if false   <-- syntax
 	
-	
-
-
-
+	( n%2==0) ? std::cout << "Even" : std::cout << "odd" ;
 }
