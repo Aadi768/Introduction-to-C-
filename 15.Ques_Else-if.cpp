55ea376f002a2be3a900a11059086c7399271cc0
Aadi768 <aadityahridaya2205@gmail.com>
2024-12-29 11:12:42 +0530

    Update and rename 15.Ques.cpp to 15.Ques_Else-if.cpp

---
+++ 15.Ques_Else-if.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+	
+	float x ;
+	std::cout << "Enter the Marks .: " ;
+	std::cin >> x ;
+	
+	if ( x<=100 and  x>=81 ) { 
+		std::cout << "Very Good" ;
+	}
+	else if ( x<=80 and x>=61 ) {
+		std::cout << "Good" ;
+	}
+	else if  (  x<=60 and x>= 41 ) {
+		std::cout << "Average" ;
+	}
+	else if ( x>=0 and x<=40 ) {
+		std::cout << "Fail" ;
+	}
+	else {
+		std::cout << "Error" ;
+	}
+}
+ return 0;
+}
