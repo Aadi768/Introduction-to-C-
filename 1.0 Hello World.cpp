e9c0e59aa16cd15f0feeac1076145470e453305c
Aadi768 <aadityahridaya2205@gmail.com>
2025-01-13 21:54:20 +0530

    Update and rename 1.0Hello World.cpp to 1.0 Hello World.cpp

---
+++ 1.0 Hello World.cpp
@@ -0,0 +1,7 @@
+#include<iostream>                       // you have to write the fist two line
+int main(){
+	std::cout << "Hello World!" << std::endl;   // cout stand for output or printing  
+						    // "<<" for next instruction 
+						    // endl for next line ( enter basically)
+	return 0;
+}
