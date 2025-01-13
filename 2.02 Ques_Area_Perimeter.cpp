2dc1c192130407b7ca85b06f1c0deec074a91817
Aadi768 <aadityahridaya2205@gmail.com>
2025-01-13 22:22:32 +0530

    Update and rename 2.2 Ques_Area_Perimeter.cpp to 2.02 Ques_Area_Perimeter.cpp

--- 2.02 Ques_Area_Perimeter.cpp
+++ 2.02 Ques_Area_Perimeter.cpp
@@ -1,14 +1,14 @@
-/* Ques - Given the length and breadth of a rectangle, WAP to find wheather 
+/* Ques - Given the length and breadth of a rectangle, WAP to find whether 
 	the area of the rectangle is greater than its perimeter */ 
 #include <iostream>
 
 int main (){
 	float l ;
-	std::cout << "Enter Length of Reactangle .: " ;
+	std::cout << "Enter Length of Rectangle .: " ;
 	std::cin >> l ;
 	
 	float b ;
-	std::cout << "Enter Width of Reactangle .: " ;
+	std::cout << "Enter Width of Rectangle .: " ;
 	std::cin >> b ;
 	
 	float peri = 2*l*b ;
